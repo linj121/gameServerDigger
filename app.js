5871211e1bada4748c12ea3bc504fb6da5224511
linj121 <jiexulin99@gmail.com>
2024-03-31 07:14:27 -0400

    feat: removed unreachable servers

--- app.js
+++ app.js
@@ -1,62 +1,62 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const logger = require('morgan')
-const app = express()
-const PORT = 3000
+const express = require("express");
+const logger = require("morgan");
+const app = express();
+const PORT = 3000;
 
-const { GameDig } = require('gamedig')
+const { GameDig } = require("gamedig");
 
 const sockets = [
-    "103.219.30.118:11543",
-    "103.219.30.118:12963",
-    "103.219.30.118:13433",
-    "103.219.30.118:18560",
-    "103.219.30.118:19863",
-    "103.219.30.118:22716",
-    "106.55.254.89:27015",
-    "103.219.39.249:30040",
-    "103.219.39.249:32537",
-    "103.219.39.249:30452",
-    "49.232.122.146:27015",
-    "49.232.122.146:27016",
-    "106.55.254.89:27016",
-    "103.219.30.124:27099",
-    "103.219.30.124:26620",
-    "103.219.30.118:25417",
-    "103.219.39.249:37739"
-]
+  "103.219.30.118:11543",
+  "103.219.30.118:12963",
+  "103.219.30.118:13433",
+  "103.219.30.118:18560",
+  "103.219.30.118:19863",
+  "103.219.30.118:22716",
+  "106.55.254.89:27015",
+  "103.219.39.249:30040",
+  "103.219.39.249:32537",
+  "103.219.39.249:30452",
+  "49.232.122.146:27015",
+  "49.232.122.146:27016",
+  "106.55.254.89:27016",
+  //"103.219.30.124:27099",
+  //"103.219.30.124:26620",
+  "103.219.30.118:25417",
+  "103.219.39.249:37739",
+];
 
 async function getServerList(list) {
-    const result = {}
-    const queries = list.map(async (server) => {
-        const [host, port] = server.split(":")
-        try {
-            const queryResult = await GameDig.query({
-                type: "l4d2",
-                host,
-                port
-            })
-            result[server] = queryResult
-        } catch (error) {
-            console.log("Error getting info from server: ", server)
-        }
-    })
-
+  const result = {};
+  const queries = list.map(async (server) => {
+    const [host, port] = server.split(":");
     try {
-        await Promise.allSettled(queries)
-        return result
+      const queryResult = await GameDig.query({
+        type: "l4d2",
+        host,
+        port,
+      });
+      result[server] = queryResult;
     } catch (error) {
-        console.error(error)
+      console.log("Error getting info from server: ", server);
     }
+  });
+
+  try {
+    await Promise.allSettled(queries);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-app.use(logger('dev'))
-app.get('/', (req, res) => res.send('Puniavo!'))
+app.use(logger("dev"));
+app.get("/", (req, res) => res.send("Puniavo!"));
 
-app.get('/servers', async (req, res) => {
-    const serverList = await getServerList(sockets)
-    res.json(serverList)
-})
+app.get("/servers", async (req, res) => {
+  const serverList = await getServerList(sockets);
+  res.json(serverList);
+});
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
